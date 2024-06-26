1dcd60a0b2cf73a13d2a06ae8e49456df4ff232a
FeliCres7 <48242703@est.ort.edu.ar>
2024-06-26 14:12:43 -0300

    desarrollo el codigo y agrego valores de precio

--- Assets/visualizadorscript.cs
+++ Assets/visualizadorscript.cs
@@ -4,16 +4,41 @@ using UnityEngine;
 
 public class visualizadorscript : MonoBehaviour
 {
-    public GameObject[] producto;
+    public GameObject[] producto1;
+    public GameObject[] producto2;
+    int factorderecho;
+    int factorizquierdo;
     // Start is called before the first frame update
     void Start()
     {
-        
+        DeactivateAll();
+        Activate();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void DeactivateAll()
+    {
+    for (int i=0; i<producto1.Length; i++)
+        {
+            producto1[i].SetActive(false);
+            producto2[i].SetActive(false);
+        }
+    }
+
+    void Activate()
+    {
+        factorizquierdo = Random.Range(0, producto1.Length);
+        factorderecho = Random.Range(0, producto2.Length);
+        GameObject objetoAMover1 = producto1[factorizquierdo];
+        Vector3 nuevaPosicion1 = new Vector3(50, 0, 0);
+        GameObject objetoAMover2 = producto2[factorderecho];
+        Vector3 nuevaPosicion2 = new Vector3(-50, 0, 0);
+        producto1[factorizquierdo].SetActive(true);
+        producto2[factorderecho].SetActive(true);
     }
 }
